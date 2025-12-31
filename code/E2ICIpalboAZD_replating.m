ae266de8905016524ebdc47754d5d5b3e0ad0ecb
Wei He <heweivt@gmail.com>
2025-12-31 12:59:05 -0500

    Delete code/E2ICIpalboAZD_replating.m

--- code/E2ICIpalboAZD_replating.m
+++
@@ -1,8 +0,0 @@
-function x = E2ICIpalboAZD_replating(x,args)
-%% (6) Function index in the GitHub repository.
-% This function resets the initial cell number each month, simulating the 
-% replating done in the experiment.
-
-x(end - 1) = 1 - args.PAR(args.ind_apopratioT0);% alive cells
-x(end) = args.PAR(args.ind_apopratioT0);% apoptosis cells
-end
